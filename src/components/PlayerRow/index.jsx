b9fb76cdc411bc195dee7bf7cf421b8088e6098b
Phil Ma <phillio411@gmail.com>
2019-12-17 16:14:10 -0500

    render player stats table, need to refactor css

--- src/components/PlayerRow/index.jsx
+++ src/components/PlayerRow/index.jsx
@@ -3,17 +3,45 @@ import React from "react";
 import "./PlayerRow.css";
 
 const PlayerRow = (props) => {
-    console.log('props', props)
+    // console.log('props',props)
+    const playerStats = props.stats
+    const playerStatsKeys = Object.keys(playerStats)
+    const statsHolder = props.statsHolder
     return(
-        <ul className={props.id}>
-            <li>{props.name}</li>
-            <li>{props.position}</li>
-            <li>{props.teamAbbr}</li>
-            <li>{props.seasonPts}</li>
-            <li>{props.seasonProjectedPts}</li>
-            <li>{props.weekPts}</li>
-            <li>{props.weekProjectedPts}</li>
-        </ul>
+        <tr className="player-card" id={`player-${props.id}`}>
+            <td className="player-name" >{props.name}</td>
+            <td className="player-position" >{props.position}</td>
+            <td className="player-teamAbbr" >{props.teamAbbr}</td>
+            <td className="player-seasonPts" >{props.seasonPts}</td>
+            <td className="player-seasonProjectedPts" >{props.seasonProjectedPts}</td>
+            <td className="player-weekPts" >{props.weekPts}</td>
+            <td className="player-weekProjectedPts" >{props.weekProjectedPts}</td>
+
+            {statsHolder.map(stat => {
+                if (playerStats[stat.id] !== undefined) {
+                    return <td className={`player-stats player-stats-${stat.id} col-${stat.id}`} >{playerStats[stat.id]}</td>
+                } else {
+                    return <td className={`player-stats player-stats-${stat.id} col-${stat.id}`} >-</td>
+                }
+            })}
+
+            {/* {props.statsHolder.map(stat => {
+                return (
+                    <td className={`player-stats-${stat.id}`}>-</td>
+                )
+            })}
+            {props.stats.map(stat => {
+                const statDiv = document.querySelector(`player-stats-${stat.id}`)
+                return statDiv.innerHTML(`${stat.value}`)
+            })} */}
+
+
+            {/* {props.statsHolder.map(stat => {
+                return (
+                    <td className="player-stats">-</td>
+                )
+            })} */}
+        </tr>
     )
 }
 
---
+++ src/components/PlayerRowHeader/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import "./PlayerRowHeader.css";
+
+const PlayerRowHeader = (props) => {
+    // console.log('props',props)
+    return(
+            <td className={`header-abbr col-${props.header.id}`} id={`header-abbr-${props.header.id}`} >{props.header.abbr}</td>
+    )
+}
+
+export default PlayerRowHeader;
+
+
+            // {this.props.statsHolder.map(stat => {
+            //     return (
+            //         <td className="player-stats"></td>
+            //     )
+            // })}
+
+
+        // <div className="header-card" id={`header-${props.id}`}>
+        //     <td className="header-position" >{props.position}</td>
+        //     <td className="header-teamAbbr" >{props.teamAbbr}</td>
+        //     <td className="header-seasonPts" >{props.seasonPts}</td>
+        //     <td className="header-seasonProjectedPts" >{props.seasonProjectedPts}</td>
+        //     <td className="header-weekPts" >{props.weekPts}</td>
+        //     <td className="header-weekProjectedPts" >{props.weekProjectedPts}</td>
+        // </div>
